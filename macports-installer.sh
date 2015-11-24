77ac8da136360d034d82795cf0394dc74668a823
tjt263 <tjt263@gmail.com>
2015-11-24 05:54:09 +0000

    First commit

---
+++ macports-installer.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+urisvn=https://svn.macports.org/repository/macports/trunk/www/includes/common.inc
+uridist=https://distfiles.macports.org/MacPorts
+#
+mpvers=$(curl -s $urisvn | grep -m 1 -e \'*.*.*\' | cut -d \' -f 2)
+osmajor=$(uname -r | cut -d . -f 1)
+#
+case $osmajor in
+8)
+    osvers=10.4
+    osname=Tiger
+    ;;
+9)
+    osvers=10.5
+    osname=Leopard
+    ;;
+10)
+    osvers=10.6
+    osname=SnowLeopard
+    ;;
+11)
+    osvers=10.7
+    osname=Lion
+    ;;
+12)
+    osvers=10.8
+    osname=MountainLion
+    ;;
+13)
+    osvers=10.9
+    osname=Mavericks
+    ;;
+14)
+    osvers=10.10
+    osname=Yosemite
+    ;;
+15)
+    osvers=10.11
+    osname=ElCapitan
+    ;;
+*)
+    echo Warning: unknown OS version
+    ;;
+esac
+#
+mppkg=MacPorts-${mpvers}-${osvers}-${osname}
+mpchk=MacPorts-${mpvers}
+echo $mppkg.pkg
+echo $mpchk.chk.txt
+curl $uridist/$mppkg.pkg -O
+curl $uridist/$mpchk.chk.txt -O
+sha256=$(shasum -a 256 ./$mppkg.pkg)
+#
+if grep $sha256 $mpchk.chk.txt;
+    then sudo installer -verboseR -allowUntrusted -pkg ./$mppkg.pkg -target /;
+fi
+#
+rm ./$mppkg.pkg
+rm ./$mpchk.chk.txt
+#
+if ! grep -F 'export PATH="/opt/local/bin:/opt/local/sbin:$PATH"' .bash_profile;
+    then echo 'export PATH="/opt/local/bin:/opt/local/sbin:$PATH"' >> .bash_profile;
+fi
+#
